5fcea153165927ac572cfc9c4dfc8b2616eec5d5
jaydangar <jayjaydangar96@gmail.com>
2025-01-13 16:55:17 -0500

    Adding AutoScaling through ELB, Target-Groups and ASG

--- asg.tf
+++ asg.tf
@@ -12,11 +12,70 @@ resource "aws_launch_template" "launch-configuration" {
   }
 }
 
+resource "aws_lb" "load-balancer" {
+  name = "auto-load-balancer"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http-listener" {
+  load_balancer_arn = aws_lb.load-balancer.arn
+  port = var.http_allowlisted_ports
+  protocol = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "listener-rule" {
+  listener_arn = aws_lb_listener.http-listener.arn
+  priority = 100
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target-group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_lb_target_group" "target-group" {
+  name = "target-group"
+  port = var.http_allowlisted_ports
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
 resource "aws_autoscaling_group" "autoscaling-group" {
   launch_template {
     id = aws_launch_template.launch-configuration.id
     version = "$Latest"
-  } 
+  }
+  health_check_type = "ELB"
+  target_group_arns = [aws_lb_target_group.target-group.arn]
   vpc_zone_identifier = data.aws_subnets.default.ids
   min_size = 2
   max_size = 5
@@ -73,6 +132,22 @@ resource "aws_security_group" "ec2-https-egress-rule" {
   }
 }
 
+resource "aws_security_group" "alb" {
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 variable "ssh_allowlisted_ports" {
   description = "ssh ingress and egress allowlisted ports"
   default = 22
@@ -106,4 +181,8 @@ data "aws_subnets" "default" {
 
 output "default_subnet_ids" {
   value = data.aws_subnets.default.ids
+}
+
+output "elb_id" {
+  value = aws_lb.load-balancer.dns_name
 }
